664052e9c00a0eb552106888af3a2b86e603dcb6
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2021-11-30 09:38:19 -0300

    atualização

---
+++ calculando-compras/script.js
@@ -0,0 +1,10 @@
+function ok(){
+    if(nvalor1 == nvalor2){
+    window.alert("jfc")
+    }
+}
+
+var num1= document.getElementById("valor1")
+    var nvalor1= Number('num1')
+    var num2= document.getElementById("valor2")
+    var nvalor2= Number('num2')
