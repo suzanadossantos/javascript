819e9ab4dc772dad7bd2ac0093e37190ecad2d3b
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2021-12-01 09:12:38 -0300

    calculando

--- calculando-compras/script.js
+++ calculando-compras/script.js
@@ -1,12 +1,16 @@
 function ok(){
     var res=document.querySelector("div#res")
+
     var num1= document.getElementById("valor1")
     var nvalor1= Number(num1.value)
+
     var num2= document.getElementById("valor2")
     var nvalor2= Number(num2.value)
+
+    res.innerHTML="<p></p>"
     
-    if(nvalor2 == 0){
-    res.innerHTML+="jfc"
+    if(nvalor1 == nvalor2){
+    res.innerHTML="<p></p>"
     }
 }
 
