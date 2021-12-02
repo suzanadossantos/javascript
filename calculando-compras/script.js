89d2a72eed23b99833cc9c393d42b4c81d128d30
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2021-12-02 10:24:08 -0300

    atualizações nos arquivos

--- calculando-compras/script.js
+++ calculando-compras/script.js
@@ -1,16 +1,23 @@
 function ok(){
+    var valor1 = document.getElementById("valor1")
+    var valor2= document.getElementById("valor2")
     var res=document.querySelector("div#res")
+    var var1=Number(valor1.value)
+    var var2=Number(valor2.value)
 
-    var num1= document.getElementById("valor1")
-    var nvalor1= Number(num1.value)
+    var resultado=var2-var1
+    var sobra = var1 - var2
 
-    var num2= document.getElementById("valor2")
-    var nvalor2= Number(num2.value)
-
-    res.innerHTML="<p></p>"
-    
-    if(nvalor1 == nvalor2){
-    res.innerHTML="<p></p>"
+    if((resultado!=0) && (resultado>=0)){
+        res.innerHTML=`<p>Você tem dinheiro o suficiente para efetuar a compra!</p>`
+        res.innerHTML+=`<p>Sobrou: $${resultado.toFixed(2)}</p>`
+    }else if(resultado>=1){
+        res.innerHTML=`<p>Infelizmente você não tem dinheiro o suficiente para efetuar a compra.</p>`
+        res.innerHTML+=`<p>Você precisa de mais: $${sobra.toFixed(2)}</p>`
     }
 }
 
+function reset(){
+    var res=document.querySelector("div#res")
+    res.innerHTML="<p>dsdddd</p>"
+}
