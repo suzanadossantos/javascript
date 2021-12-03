9381b7aa5779e72d2fd6af8ee4890cd01caa27f1
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2021-12-03 09:14:32 -0300

    formulario

--- formulario/script.js
+++ formulario/script.js
@@ -1,3 +1,13 @@
 function ok(){
-    
+    var nome= document.querySelector("input#nome")
+    var telefone= document.querySelector("input#tel")
+    var email= document.querySelector("input#email")
+
+    if(nome.value == ""){
+        alert("Nome não informado")
+    }else if(telefone.value == 0){
+        alert("Telefone não informado")
+    }else if(email.value == ""){
+        alert("Email não informado")
+    }
 }
