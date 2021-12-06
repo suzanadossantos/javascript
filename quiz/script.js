a9333abb2e5c9608f8025633f56507931d834aa1
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2021-12-06 12:06:58 -0300

    quiz

--- quiz/script.js
+++ quiz/script.js
@@ -2,16 +2,15 @@ function ok(){
     var q1=document.querySelector("input#q1r-2")
     var q2=document.querySelector("input#q2r-3")
     var q3=document.querySelector("input#q3r-1")
-    var res= document.querySelector("div#res")
-
+    
     if((q1.checked) && (q2.checked) && (q3.checked)){
         alert("Você acertou todas as questões!")
     } else if((q1.checked) && (q2.checked)){
-        alert("Você acertou a primeira e a segunda questão")
+        alert("Você acertou a primeira e a segunda questão!")
     } else if((q1.checked) && (q3.checked)){
-        alert("Você acertou a primeira e a terceira questão")
+        alert("Você acertou a primeira e a terceira questão!")
     }else if((q2.checked) && (q3.checked)){
-        alert("Você acertou a segunda e a terceira questão")
+        alert("Você acertou a segunda e a terceira questão!")
     } else if(q1.checked){
         alert("Você acertou a primeira questão!")
     }else if(q2.checked){
