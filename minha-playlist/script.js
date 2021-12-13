2345570c944048e4c5110b397c0c285322e41ab8
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2021-12-13 08:56:14 -0300

    criando exercício

---
+++ minha-playlist/script.js
@@ -0,0 +1,8 @@
+function ok(){
+    var nome= document.getElementById('musica')
+    var escr= document.querySelector("div#music")
+
+    if(nome== 'ff'){
+        escr.innerHTML="<p>fvdgv</p>"
+    }
+}
