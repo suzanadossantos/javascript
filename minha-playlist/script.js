3910479345217aa5dffb88e42276f3fc12e40b6a
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2021-12-13 09:51:20 -0300

    atualizando

--- minha-playlist/script.js
+++ minha-playlist/script.js
@@ -1,8 +1,18 @@
 function ok(){
-    var nome= document.getElementById('musica')
-    var escr= document.querySelector("div#music")
+    var nome= document.getElementById('musica').value
+    var adic= document.querySelector("div#music")
 
-    if(nome== 'ff'){
-        escr.innerHTML="<p>fvdgv</p>"
-    }
+    if(nome != ""){
+        adic.innerHTML+="<ul><ul>"
+        adic.innerHTML+=`<li>${nome}</li>`
+    }else{
+        alert("Nada foi adicionado!")
+    }  
 }
+//max-width: 600px;
+    //padding: 2%;
+    //margin: auto;
+    //background-color: rgba(43, 40, 39, 0.589);
+    //text-align: center;
+    //border-radius: 10px;
+    //border: 1px solid;
