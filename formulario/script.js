9bbff9c1d381966b1081ae5ba2d7b7ee09359125
Suzana dos Santos <94690066+suzanadossantos@users.noreply.github.com>
2022-06-21 09:38:33 -0300

    formulario

--- formulario/script.js
+++ formulario/script.js
@@ -1,15 +1,30 @@
-function ok(){
-    var nome= document.querySelector("input#nome")
-    var telefone= document.querySelector("input#tel")
-    var email= document.querySelector("input#email")
+function validar(){
+    var nome = formulario.nome;
+    var sobrenome = formulario.sobrenome;
+    var email = formulario.email;
+    var telefone = formulario.telefone;
 
     if(nome.value == ""){
         alert("Nome não informado")
-    }else if(telefone.value == 0){
+
+        nome.focus();
+    }
+
+    if(sobrenome.value == ""){
+        alert("Sobrenome não informado")
+
+        sobrenome.focus();
+    }
+
+    if(email.value == ""){
+        alert("E-mail não informado")
+
+        email.focus();
+    }
+
+    if(telefone.value == ""){
         alert("Telefone não informado")
-    }else if(email.value == ""){
-        alert("Email não informado")
-    }else{
-        alert("Formulário enviado!")
+
+        telefone.focus();
     }
 }
